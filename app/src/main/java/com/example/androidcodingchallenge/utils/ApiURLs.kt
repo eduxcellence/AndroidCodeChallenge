6a28e143879e57e9b6864b8e8e28253ceaafcd3f
eduxcellence <akkr2017@gmail.com>
2023-02-27 03:06:30 +0530

    Constants

---
+++ app/src/main/java/com/example/androidcodingchallenge/utils/ApiURLs.kt
@@ -0,0 +1,13 @@
+package com.example.androidcodingchallenge.utils
+
+/**
+ * Created by Akash Kumar on 26/02/23.
+ * https://github.com/eduxcellence
+ * akkr2017@gmail.com
+ */
+/**
+* URL Const decalrations for Global Scope
+* */
+object ApiURLs {
+    const val searchQuery = "software/acromine/dictionary.py"
+}
---
+++ app/src/main/java/com/example/androidcodingchallenge/utils/AppConstants.kt
@@ -0,0 +1,15 @@
+package com.example.androidcodingchallenge.utils
+/**
+ * Created by Akash Kumar on 25/02/23.
+ * https://github.com/eduxcellence
+ * akkr2017@gmail.com
+ */
+/**
+ * App Const decalrations for Global Scope
+ * */
+object AppConstants{
+    const val REQUIRED_FIELD = "required field"
+    const val MIN_KEYWORD_VALIDATION = "required at least 3 words"
+    const val VALID = "valid"
+    const val NO_INTERNET_CONNECTION = "No Internet Connection"
+}
